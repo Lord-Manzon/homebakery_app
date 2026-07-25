379af6c8966656aa94ed7e03c0ee90765a130ad0
lord manzon <lordmanzon17@gmail.com>
2026-07-26 02:22:18 +0800

    feat: add pieces_per_variant field to ProductVariant type

--- src/types/index.ts
+++ src/types/index.ts
@@ -33,6 +33,7 @@ export type ProductVariant = {
   selling_price: number;
   packaging: string | null;
   packaging_cost: number;
+  pieces_per_variant: number;
   is_archived: boolean;
   created_at: string;
   updated_at: string;
